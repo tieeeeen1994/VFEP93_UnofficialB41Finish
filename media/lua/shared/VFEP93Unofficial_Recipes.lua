9404c86a0137ede5b3ae5c992c1b079727ac6886
Tien <tieeeeen1994@gmail.com>
2025-05-10 03:18:34 +0800

    Fix issues again regarding the ammo maker recipes not appearing

--- media/lua/shared/VFEP93Unofficial_Recipes.lua
+++ media/lua/shared/VFEP93Unofficial_Recipes.lua
@@ -1,14 +1,17 @@
 Events.OnGameStart.Add(function()
     if getActivatedMods():contains("ammomaker") then
         local recipesToUnhide = {
-            "Base.Make 5.7mm Ammo [VFEP93]",
-            "Base.Make 4.6mm Ammo [VFEP93]",
+            "Base.Make 5.7x28mm [VFE]",
+            "Base.Make 4.6x30mm [VFE]",
         }
 
         for _, recipeName in ipairs(recipesToUnhide) do
             local recipe = getScriptManager():getRecipe(recipeName)
             if recipe then
                 recipe:setIsHidden(false)
+                print("Unhid recipe: " .. recipeName)
+            else
+                print("Recipe not found: " .. recipeName)
             end
         end
     end
